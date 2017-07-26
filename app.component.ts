6a5bc6f54047bd0ebd0910f05c89122fc1f00bd6
Rahul Tiwari <30317222+rahultiwari2908@users.noreply.github.com>
2017-07-26 16:08:53 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,17 @@
+import { Component }          from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">User</a>
+      <a routerLink="/heroes" routerLinkActive="active">Admin</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Login As';
+}
